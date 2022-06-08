40057dfe4c9a10143c24df9c3eebd37baa680696
KietNGer <101463615+KietNGer@users.noreply.github.com>
2022-06-08 22:42:56 +0700

    Add files via upload

---
+++ Do_min_BT_chinh/DoMin/main.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include "Header.h"
+#include "Console.h"
+#include "Functions.h"
+#include<Windows.h>
+using namespace std;
+
+
+int main()
+{
+	SetWindowSize(160, 50);
+	SetFontSize(10);
+	SetScreenBufferSize(160, 50);
+	DisableCtrButton(0, 1, 1);
+	DisableResizeWindow();
+	showScrollbar(false);
+	ShowCur(false);
+	resizeConsole(CSWidth, CSHeight);
+	SetConsoleTitle(TEXT("GAME DÒ MÌN - OBJECT ORIENTED PROGRAMMING"));
+	CTBang.designTittle();
+	Cursor(false, 1);
+	CTBang.mainMenu(0);
+	Trang = 1;
+	CTBang.xuLy();
+	return 0;
+}
